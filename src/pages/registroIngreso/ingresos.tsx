b5c2f192dbaf5935229cf277f49e2417c7ba06ab
Abdon <estadia2020@gmail.com>
2023-08-30 12:43:53 -0600

    añadiendo nuevos campos al nav

--- src/pages/registroIngreso/ingresos.tsx
+++ src/pages/registroIngreso/ingresos.tsx
@@ -7,7 +7,12 @@ import fn                       from "../../utility";
 import fng                      from "../../components/atoms/ingresos/funciones";
 import { message }              from "antd";
 import dayjs                    from "dayjs";
-import type { DatePickerProps } from "antd";
+import SearchIcon               from "@mui/icons-material/Search";
+import IconButton               from "@mui/material/IconButton";
+import InputBase                from "@mui/material/InputBase";
+import FileDownloadIcon         from '@mui/icons-material/FileDownload';
+import Button                   from "@mui/material/Button";
+//import * as XLSX                from 'xlsx/xlsx.mjs';
 import { TableCustom }          from "../../components/molecules/table/tableCustom";
 import { ModalBank }            from '../../components/organims/modal-bank'
 //import { IngresoResponsive } from './IngresoResponsive.tsx';
@@ -138,18 +143,6 @@ if (user_id !== "" && user_id !== null) {
   cargarDatosIngresos();
 }
 
-/*const onChange: DatePickerProps["onChange"] = (date, dateString) => {
-  setInitialValues({
-    hdId:                   fn.obtenerValor("#hdId"),
-    txtNombre:              fn.obtenerValor("#txtNombre"),
-    txtConcepto:            fn.obtenerValor("#txtConcepto"),
-    stTipo:                 fn.obtenerValor("#stTipo"),
-    stCategoria:            fn.obtenerValor("#stCategoria"),
-    txtMonto:               fn.obtenerValor("#txtMonto"),
-    txtFechaTentativaCobro: dayjs(dateString),
-  });
-};*/
-
 let idSI = setInterval(() => {
   if (!data) console.log("Vacio");
   else {
@@ -165,8 +158,86 @@ let idSI = setInterval(() => {
 
 const buscarPorSelect = () => {
   cargarDatosIngresos(false,setListaDatos);
+  setStMetodo(parseInt(fn.obtenerValor("#stTipoB")));
+  setStEstado(parseInt(fn.obtenerValor("#stEstadoB")));
 }
- 
+
+/*const onChange: DatePickerProps["onChange"] = (date, dateString) => {
+  setInitialValues({
+    hdId:                   fn.obtenerValor("#hdId"),
+    txtNombre:              fn.obtenerValor("#txtNombre"),
+    txtConcepto:            fn.obtenerValor("#txtConcepto"),
+    stTipo:                 fn.obtenerValor("#stTipo"),
+    stCategoria:            fn.obtenerValor("#stCategoria"),
+    txtMonto:               fn.obtenerValor("#txtMonto"),
+    txtFechaTentativaCobro: dayjs(dateString),
+  });
+};*/
+
+/* ######################################### */
+const cobrar = () => {
+  const scriptURL           = localStorage.getItem('site')+"/cambiarCobrado"; // deberia es
+  const ingresos_futuros_id = idIngresoStatus;
+  const dataU               = {ingresos_futuros_id};
+  setConfirm2Loading(true)
+  fetch(scriptURL, {
+     method: 'POST',
+     body:    JSON.stringify(dataU),
+     headers:{
+       'Content-Type': 'application/json'
+     }
+   })
+  .then((resp) => resp.json())
+  .then(function(info) {
+    cargarDatosIngresos(true,setListaDatos);
+    setTimeout(()=> {
+      setModal2Open       (false);
+      setConfirm2Loading  (false)
+    },600)
+  })
+  .catch(error => {
+    console.log   (error.message);
+    console.error ('Error!', error.message);
+  });
+};
+
+const revertir = () => {
+  const scriptURL           = localStorage.getItem('site')+"/revertirCobro"; // deberia es
+  const ingresos_futuros_id = idIngresoStatus;
+  const dataU               = {ingresos_futuros_id};
+  setConfirm2Loading(true)
+  fetch(scriptURL, {
+     method:  'POST',
+     body:    JSON.stringify(dataU),
+     headers:{
+       'Content-Type': 'application/json'
+     }
+   })
+  .then((resp) => resp.json())
+  .then(function(info) {
+    cargarDatosIngresos(true, setListaDatos);
+    setTimeout(()=> {
+      setModal2Open       (false);
+      setConfirm2Loading  (false)
+    },600)
+  })
+  .catch(error => {
+    console.log   (error.message);
+    console.error ('Error!', error.message);
+  });
+};
+
+/*
+const handleOnExcel = () => {
+  var wb  = XLSX.utils.book_new(),
+  ws      = XLSX.utils.json_to_sheet(listData);
+
+  XLSX.utils.book_append_sheet  (wb,ws,"IngresosFuturos");
+  XLSX.writeFile                (wb,"IngresosFuturos.xlsx");
+}
+*/
+
+
 return (
   <Box>
     <Box    className = {Styles.nav}>
@@ -177,39 +248,89 @@ return (
         </div>
       </Box>
 
-      <Box  className = {Styles.itemSearch}>
+      <Box className={Styles.itemSearch}>
+        <Paper
+          // component="form"
+          sx={{
+            display:    "flex",
+            alignItems: "center",
+          }}
+          className     = "BorderContenedor"
+        >
+          <InputBase
+            id          = "txtSearch"
+            name        = "txtSearch"
+            sx          = {{ ml: 1, flex: 1 }}
+            placeholder = "Buscar"
+            inputProps  = {{ "aria-label": "search google maps" }}
+            onKeyUp     = {() => {
+              fn.ejecutarClick("#btnBuscar");
+            }}
+          />
+          <IconButton
+            id          = "btnBuscar"
+            type        = "button"
+            sx          = {{ p: "10px" }}
+            aria-label  = "search"
+            onClick     = {() => {
+              cargarDatosIngresos(true, setListaDatos);
+              setStMetodo(0);
+              setStEstado(0);
+            }}
+          >
+            <SearchIcon />
+          </IconButton>
+        </Paper>
+      </Box>
+
+      <Box className={Styles.itemSearch}>
         <Paper
           sx={{
             display:    "flex",
             alignItems: "center",
           }}
         >
-        <label htmlFor="stTipoB" className={Styles.LblFilter}>Método</label>
-        <select
-          name        = "stTipoB"
-          id          = "stTipoB"
-          className   = {`${Styles.ModalSelect} ${Styles.ModalSelectBrVerde}`}
-          onChange    = {buscarPorSelect}
-        >
-          <option value="0">Todos         </option>
-          <option value="1">Efectivo      </option>
-          <option value="2">Transferencia </option>
-        </select>
+          <label htmlFor="stTipoB" className={Styles.LblFilter}>Método</label>
+          <select
+            name      = "stTipoB"
+            id        = "stTipoB"
+            className = {`${Styles.ModalSelect} ${Styles.ModalSelectBrVerde}`}
+            onChange  = {buscarPorSelect}
+            value     = {stMetodo}
+          >
+            <option value="0">Todos         </option>
+            <option value="1">Efectivo      </option>
+            <option value="2">Transferencia </option>
+          </select>
 
-        <label htmlFor="stEstadoB" className={Styles.LblFilter}>Estado</label>
-        <select
-          name      = "stEstadoB"
-          id        = "stEstadoB"
-          className = {`${Styles.ModalSelect} ${Styles.ModalSelectBrVerde}`}
-          onChange  = {buscarPorSelect}
-        >
-          <option value="0">Todos       </option>
-          <option value="1">Cobrados    </option>
-          <option value="2">No cobrados </option>
-        </select>
-
+          <label htmlFor="stEstadoB" className={Styles.LblFilter}>Estado</label>
+          <select
+            name      = "stEstadoB"
+            id        = "stEstadoB"
+            className = {`${Styles.ModalSelect} ${Styles.ModalSelectBrVerde}`}
+            onChange  = {buscarPorSelect}
+            value     = {stEstado}
+          >
+            <option value="0">Todos       </option>
+            <option value="1">Cobrados    </option>
+            <option value="2">No cobrados </option>
+          </select>
         </Paper>
       </Box>
+
+      <Box className={Styles.itemButton}>
+        <Button
+          variant   = "contained"
+          color     = "success"
+          startIcon = {<FileDownloadIcon />}
+          classes   = {{
+            root: Styles.btnCreateAccount,
+          }}
+          //onClick   = {handleOnExcel}
+        >
+          Exportar a excel
+        </Button>
+      </Box>  
 
       <ModalBank
         namePerson          = {true}
